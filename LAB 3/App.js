406b1cbf6a10b3b49eafd955c282b27a170e23f8
Avish787 <avishbhati2000@gmail.com>
2026-09-08 09:47:42 +0530

    changes

--- LAB 3/App.js
+++ LAB 3/App.js
@@ -1,10 +1,20 @@
 import http from "http";
+import * as teams from 'teams.js'
+
+const PORT = 5000;
 
 const server = http.createServer((req,res)=> {
-    res.end("<h1 style = color:blue;>Welcome to Server Side</h1>");
+
+   if(req.url ==='/' && req.method == 'GET'){
+    const teams = teams.getAllTeams();
+    res.write(JSON.stringify(teams));
+    return teams;
+   }else{
+    res.statusCode = 404;
+   }
 });
 
-server.listen(5000,()=>{
-    console.log("Server is running");
+server.listen(PORT,()=>{
+    console.log("SIH Server is running at ",PORT);
 });
 
