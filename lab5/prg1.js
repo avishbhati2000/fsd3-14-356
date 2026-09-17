b590d130e4cf17e9705f2abe95f7a09adb537922
Avish787 <avishbhati2000@gmail.com>
2026-09-17 14:34:17 +0530

    changes in prg1.js and made webpages

--- lab5/prg1.js
+++ lab5/prg1.js
@@ -1,6 +1,27 @@
 import express from "express"
+import path from 'path'
+import {fileURLToPath} from "node:url";
 
 const PORT = 3333;
 const app = express();
 
-app.listen(PORT,() => console.log("prg1 is running at ", port));
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(dirname,"public","index.html"));
+});
+
+app.get("/about",(req,res)=>{
+    res.sendFile(path.join(dirname,"public","about.html"));
+});
+
+app.get("/enquiry",(req,res)=>{
+    res.sendFile(path.join(dirname,"public","enquiry.html"));
+});
+
+app.use(express.static(path.join(dirname, "public")));
+
+app.listen(PORT,() => console.log("prg1 is running at ", PORT));
