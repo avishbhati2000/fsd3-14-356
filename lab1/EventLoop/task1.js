2c21f35ce9612e510996364628473c60bb1327a6
Avish787 <avishbhati2000@gmail.com>
2026-09-17 13:19:35 +0530

    changes in task2.js

--- lab1/EventLoop/task1.js
+++ lab1/EventLoop/task1.js
@@ -22,6 +22,10 @@ function main() {
 }
 main();
 
+
+
+
+
 //Javascript - Synchronus and single threaded form.
 //Limitation - Can't perform multiple tasks simultaneously.
 //In asynchronus we use event loop to manage the call stack.
