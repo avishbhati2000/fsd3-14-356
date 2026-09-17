f51c50fa69f24b7a6f75b1374bc9bbc9cf02fd11
Avish787 <avishbhati2000@gmail.com>
2026-09-17 13:44:52 +0530

    app.js updated

--- lab5/app.js
+++ lab5/app.js
@@ -5,4 +5,16 @@ app.get("/",(req,res)=>{
     res.send("<h1> Hello Express");
 });
 
+app.get('/about',(req,res)=>{
+    res.send("WE are FSD Developer")
+})
+
+app.get('/login',(req,res)=>{
+    res.send({msg:'user login'})
+})
+
+app.use((req,res)=>{
+    res.status(404).send("Not Found")
+})
+
 app.listen(3000, () => console.log("Server is running"));
